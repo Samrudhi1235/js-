b9d14286a475bda4b47196bbda602834f60621f9
Samrudhi1235 <samruddhikitturkar1235@gmail.com>
2026-03-12 16:01:32 +0000

    02_datatypes.js added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello"); //ReferenceError: alert is not defined// We are using node.js not browser
+
+console.log(3 
+    + 
+    
+    3);//code readiability should be high
+
+console.log()
