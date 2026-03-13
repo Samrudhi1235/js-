79455a8d1d3bb1abda6d134401cd65898bf18200
Samrudhi1235 <samruddhikitturkar1235@gmail.com>
2026-03-13 11:20:12 +0000

    03_converionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,3 +27,45 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log( typeof someNumber);
 console.log( typeof stringNumber);
+
+//******************Operations******** ***//
+
+let value =3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = " Samrudhi";
+let str2 = "Hello";
+
+let str3 = str2+str1;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2); //122
+console.log(2+2+"1"); //41
+
+console.log(((3+4)*5)%3);
+
+console.log(+true); //1
+//console.log(true+); //error
+console.log(true+1); //2
+
+let num1, num2, num3;
+num1=num2 = num3 = 2+2;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
