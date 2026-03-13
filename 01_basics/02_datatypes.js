ea2050228c0bcab1688b8b427390b03fe155f47e
Samrudhi1235 <samruddhikitturkar1235@gmail.com>
2026-03-13 10:50:12 +0000

    02_datatypes Updated

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -8,3 +8,23 @@ console.log(3
     3);//code readiability should be high
 
 console.log()
+
+
+let name = "Samrudhi";
+let age = 18;
+let isLoggedIn = false;
+
+//1.Number => 2 to power 53
+//2.bigint
+//3.string => ""
+//4.boolean =>true or false
+// null =>standalone value empty
+//undefined => abhi value assign nahi hui hai
+//symbol =>for uniqueness
+
+//Object
+
+console.log(typeof "Samrudhi");
+console.log(typeof null);
+console.log(typeof undefined);
+
